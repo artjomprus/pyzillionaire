17bb2031ce08bd112720ddd880b249660f050b0b
mk980 <malickfiaz@aol.com>
2023-11-04 10:04:49 +0200

    added app name in the settings

--- pyzillionaire/pyzillionaire/settings.py
+++ pyzillionaire/pyzillionaire/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'pyzillionaire.pyzillionaireQuiz.apps.PyzillionairequizConfig',
 ]
 
 MIDDLEWARE = [
