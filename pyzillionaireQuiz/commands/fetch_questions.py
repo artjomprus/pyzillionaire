030ae98364058f81e82df002ada1570734b1417b
Artjom <artjomprus@gmail.com>
2023-11-10 09:35:06 +0200

    Update reqs and apply postgres

---
+++ pyzillionaireQuiz/commands/fetch_questions.py
@@ -0,0 +1,18 @@
+
+from django.core.management.base import BaseCommand
+import requests
+from pyzillionaireQuiz.models import Question
+
+
+class Command(BaseCommand):
+    help = 'Fetch questions from API and store in the database'
+
+    def handle(self, *args, **kwargs):
+        api_url = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple&encode=url3986'
+        response = requests.get(api_url)
+        questions_data = response.json()
+
+        for question_data in questions_data:
+            Question.objects.create(question_text=question_data['text'])
+
+        self.stdout.write(self.style.SUCCESS('Questions fetched and stored successfully'))
