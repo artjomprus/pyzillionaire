3cef67ca8f70d235a37e5a7fa38347e3977ae30c
Artjom <artjomprus@gmail.com>
2023-11-11 10:54:30 +0200

    models.py update

--- pyzillionaireQuiz/models.py
+++ pyzillionaireQuiz/models.py
@@ -1,24 +1,121 @@
 from django.db import models
 
 
+#  Create your models here.
 class TriviaQuestion(models.Model):
-	category = models.CharField(max_length=200)
-	type = models.CharField(max_length=25)
-	difficulty = models.CharField(max_length=10)
-	question = models.TextField()
-	answer_set = models.ForeignKey('AnswerSet', null=False, blank=False, on_delete=models.CASCADE)
+    category = models.CharField(
+       max_length=200,
+       blank=False,
+       null=False
+    )
+    type = models.CharField(
+       max_length=25,
+       blank=False,
+       null=False
+    )
+    difficulty = models.CharField(
+       max_length=10,
+       blank=False,
+       null=False
+    )
+    question = models.TextField(
+       blank=False,
+       null=False
+    )
+    answer_set = models.ForeignKey(
+       'AnswerSet',
+       null=False,
+       blank=False,
+       on_delete=models.CASCADE
+    )
+    difficulty_level = models.ForeignKey(
+       'DifficultyLevel',
+       null=True,
+       blank=False,
+       on_delete=models.CASCADE
+    )
+    category_list = models.ForeignKey(
+       'CategoryList',
+       null=True,
+       blank=False,
+       on_delete=models.CASCADE
+    )
+    Amount = models.ForeignKey(
+       'Amount',
+       null=False,
+       blank=False,
+       on_delete=models.CASCADE
+    )
+
+    class AnswerSet(models.Model):
+       correct_answer = models.CharField(
+          max_length=300,
+          blank=False,
+          null=False
+       )
+       incorrect_answer_1 = models.CharField(
+          max_length=300,
+          blank=False,
+          null=False
+       )
+       incorrect_answer_2 = models.CharField(
+          max_length=300,
+          blank=False,
+          null=False
+       )
+       incorrect_answer_3 = models.CharField(
+          max_length=300,
+          blank=False,
+          null=False,
+
+       )
+       TriviaQuestion = models.ForeignKey(
+          'TriviaQuestion',
+          null=False,
+          blank=False,
+          on_delete=models.CASCADE
+
+       )
 
 
-class AnswerSet(models.Model):
-	correct_answer = models.CharField(max_length=300, blank=False, null=False)
-	incorrect_answer_1 = models.CharField(max_length=300, blank=False, null=False)
-	incorrect_answer_2 = models.CharField(max_length=300, blank=False, null=False)
-	incorrect_answer_3 = models.CharField(max_length=300, blank=False, null=False)
+class CategoryList(models.Model):
+    category_id = models.CharField(max_length=5, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    TriviaQuestion = models.ForeignKey(
+       'TriviaQuestion',
+       null=True,
+       blank=False,
+       on_delete=models.CASCADE,
+    )
 
 
-class Category:
-	pass
+class DifficultyLevel(models.Model):
+    EASY = 'easy'
+    MEDIUM = 'medium'
+    HARD = 'hard'
+    difficulty_choices = [
+       (EASY, 'easy'),
+       (MEDIUM, 'medium'),
+       (HARD, 'hard')
+    ]
+    level = models.CharField(
+       max_length=10,
+       choices=difficulty_choices,
+       default=EASY,
+       unique=True,
+       null=False,
+       blank=False)
+    TriviaQuestion = models.ForeignKey(
+       'TriviaQuestion',
+       null=True,
+       blank=False,
+       on_delete=models.CASCADE,
+    )
 
 
-class Difficulty:
-	pass
+# TriviaQuestion = models.ForeignKey('TriviaQuestion', on_delete=models.CASCADE, null=False, blank=False)
+class Amount(models.Model):
+    value = models.IntegerField()
+
+    def __str__(self):
+       return str(self.value)
